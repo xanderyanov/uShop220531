92158e20b3c23dcdba617ab211cc6be9bdce8dc6
xanderyanov <xanderyanov@gmail.com>
2022-05-30 14:48:54 +0400

    Add project files.

---
+++ uShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace uShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
