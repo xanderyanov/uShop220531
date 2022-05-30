d8e317a918016a8664b8741ec8c325104cf47b6f
xanderyanov <xanderyanov@gmail.com>
2022-05-30 16:09:12 +0400

    add_html

---
+++ uShop/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using uShop.Domain;
+
+namespace uShop.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
--- uShop/Program.cs
+++ uShop/Program.cs
@@ -1,3 +1,17 @@
+using MmxCMS;
+using MongoDB.Driver;
+using uShop.Domain;
+
+CSVtoDB CSVtoDBexport = new CSVtoDB();
+
+CSVtoDBexport.ExportCSVAsync();
+
+
+
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,3 +35,5 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+
