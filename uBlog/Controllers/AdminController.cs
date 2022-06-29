77f70d06691dda55c1ffd19f2939c2df6dca4b5e
xanderyanov <xanderyanov@gmail.com>
2022-06-29 11:03:06 +0400

    UpdatePost_prepare

--- uBlog/Controllers/AdminController.cs
+++ uBlog/Controllers/AdminController.cs
@@ -42,19 +42,56 @@ namespace uBlog.Controllers
 
         }
 
+
+        //переход на страницу, где форма обновления документа. Там все работает
         [HttpGet]
-        public IActionResult UpdatePostPage(string Id)
+        public IActionResult UpdatePost(string id)
         {
-            var postId = new ObjectId(Id);
-            
+            ObjectId Id = default;
+            try
+            {
+                Id = new ObjectId(id);
+            }
+            catch
+            {
+                return NotFound();
+            }
+
+            Post post = Data.ExistingPosts.Find(x => x.Id == Id);
+
+            return View("Update", post);
+        }
+
+        //Клик по submit отредактированной формы
+        [HttpPost]
+        public IActionResult UpdatePost(Post post)
+        {
+            if (post.Id == default)
+            {
+                var postId = post.Id;
+            }
             BsonDocument filter = new BsonDocument() {
                 {
-                    "_id", postId
+                    "_id", post.Id
                 }
             };
 
-            return View("Update", filter);
+            // Data.blogCollection.UpdateOne(filter, "то, на что заменяю - как это получить - не могу понять...");
+            // должен id сохраниться же
+            // пока получалось только создание нового документа 
+
+            //как то надо обновлять Data.ExistingPosts - чтобы в памяти актуально было все - тоже не пойму...;
+
+            return RedirectToAction("Index"); //возврат на страницу админки редактирования - где все посты
         }
+
+
+
+
+
+
+
+
 
         [HttpPost]
         public IActionResult DeletePost(string Id)
