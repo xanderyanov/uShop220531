b2d999ba686268a05262f873cb6a6d360f5388da
xanderyanov <xanderyanov@gmail.com>
2022-07-18 17:27:14 +0400

    pag_and_sort_in_blog

--- uBlog/Controllers/HomeController.cs
+++ uBlog/Controllers/HomeController.cs
@@ -7,8 +7,9 @@ namespace uBlog.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index(SortState sortOrder = SortState.DateDesc)
+        public IActionResult Index(SortState sortOrder = SortState.DateDesc, int page = 1)
         {
+            
 
             ViewData["TitleSort"] = sortOrder == SortState.TitleAsc ? SortState.TitleDesc : SortState.TitleAsc;
             ViewData["DateSort"] = sortOrder == SortState.DateAsc ? SortState.DateDesc : SortState.DateAsc;
@@ -23,7 +24,16 @@ namespace uBlog.Controllers
                 _ => Model.OrderByDescending(s => s.PostDate).ToList(),
             };
 
-            return View(Model);
+            int pageSize = 3;
+
+            var count = Model.Count();
+            var items = Model.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            PageViewModel pageViewModel = new PageViewModel(count, page, pageSize);
+            pageViewModel.Sort = sortOrder;
+            IndexViewModel viewModel = new IndexViewModel(items, pageViewModel);
+
+            return View(viewModel);
         }
     }
 }
---
+++ uBlog/Models/IndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace uBlog.Models
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Post> Posts { get; }
+        public PageViewModel PageViewModel { get; }
+        public IndexViewModel(IEnumerable<Post> posts, PageViewModel viewModel)
+        {
+            Posts = posts;
+            PageViewModel = viewModel;
+        }
+    }
+}
+
+
+//-https://metanit.com/sharp/aspnetmvc/11.7.php
