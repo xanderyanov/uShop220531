01c53a2980418a78455e0e684259a372e9b2b05a
xanderyanov <xanderyanov@gmail.com>
2022-07-19 15:44:30 +0400

    select_prepare

--- uBlog/Domain/Data.cs
+++ uBlog/Domain/Data.cs
@@ -9,10 +9,13 @@ namespace uBlog.Domain
         public static IMongoDatabase DB;
 
         public static List<Post> ExistingPosts;
+        
+        public static List<PostTopic> RamTopics;
 
         public static IMongoCollection<Post> blogCollection;
 
-        public static IMongoCollection<Flag> Flags;
+        public static IMongoCollection<PostTopic> postTopicCollection;
+
 
         public static void InitData(IConfiguration Configuration)
         {
@@ -22,9 +25,20 @@ namespace uBlog.Domain
             DB = mongoClient.GetDatabase(dbConfig.DBName);
 
             blogCollection = DB.GetCollection<Post>("blogpost");
-            Flags = DB.GetCollection<Flag>("posttags");
 
+            postTopicCollection = DB.GetCollection<PostTopic>("posttopic");
+
+            RamTopics = GetAllTopics();
             ExistingPosts = GetAllPosts();
+        }
+
+        private static List<PostTopic> GetAllTopics()
+        {
+            BsonDocument filter = new BsonDocument();
+
+            var result = postTopicCollection.Find(filter).ToList();
+
+            return result;
         }
 
         private static List<Post> GetAllPosts()
@@ -38,5 +52,6 @@ namespace uBlog.Domain
             return resultWithSortByDate.ToList();
         }
     }
+
 
 }
--- uBlog/Models/Post.cs
+++ uBlog/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using uBlog.Domain;
 
 namespace uBlog.Models
 {
@@ -26,9 +27,20 @@ namespace uBlog.Models
         public string Title { get; set; }
         public string Text { get; set; }
         public string Author { get; set; }
+        public ObjectId PostTopicId { get; set; }
+
+        public PostTopic Topic => Data.RamTopics.FirstOrDefault(x => x.Id == PostTopicId);
+
+
         public Flag Flag { get; set; }
 
         public List<Tag> Tags { get; set; }
+    }
+
+    public class PostTopic
+    {
+        public ObjectId Id { get; set; }
+        public string Title { get; set; }
     }
 
     public class Flag
@@ -38,9 +50,10 @@ namespace uBlog.Models
     }
 
     public class Tag
-    { 
+    {
         public string TagName { get; set; }
     }
+
 
     public enum SortState
     {
